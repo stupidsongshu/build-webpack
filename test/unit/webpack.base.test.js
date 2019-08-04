1259719a92b234969416938449ec743a13eed24f
squirrel <zhouyusong92@gmail.com>
2019-08-04 16:56:56 +0800

    test: webpack.base test

--- test/unit/webpack.base.test.js
+++ test/unit/webpack.base.test.js
@@ -4,7 +4,7 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base')
   // console.log(baseConfig)
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, '/Users/squirrel/repositories/geektime-courses/webpack/main/build-webpack/test/smoke/template/src/index/index.js')
-    assert.equal(baseConfig.entry.search, '/Users/squirrel/repositories/geektime-courses/webpack/main/build-webpack/test/smoke/template/src/search/index.js')
+    assert.equal(baseConfig.entry.index.indexOf('test/smoke/template/src/index/index.js') !== -1, true)
+    assert.equal(baseConfig.entry.search.indexOf('test/smoke/template/src/search/index.js') !== -1, true)
   })
 })
